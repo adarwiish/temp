0e8cecbbaedc7e4b69a6a60ff22551942af78933
adarwiish <adarwish@sandtech.com>
2024-09-19 16:38:50 +0300

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,32 +1,64 @@
+import pandas as pd
 from geopy.distance import geodesic
 import numpy as np
-from scipy.cluster.hierarchy import fclusterdata
 
-# Sample site coordinates (latitude, longitude)
-sites = [
-    (30.033333, 31.233334),  # Example: Cairo, Egypt
-    (29.976480, 31.131302),  # Giza Pyramids
-    # Add your site coordinates here
-]
+# Sample DataFrame
+data = {
+    'longitude': [106.780781, 106.780781, 106.780781, 106.780781, 106.780781, 106.780781, 106.780781, 106.780781],
+    'latitude': [-6.223201, -6.223201, -6.223201, -6.223201, -6.223201, -6.223201, -6.223201, -6.223201],
+}
+df = pd.DataFrame(data)
 
-# Distance threshold in kilometers (10 km in this case)
+# Convert DataFrame to list of tuples (lat, long)
+coords = df[['latitude', 'longitude']].to_numpy()
+
+def calculate_distance_matrix(coords):
+    """Calculate the distance matrix between all pairs of coordinates."""
+    num_sites = len(coords)
+    distance_matrix = np.zeros((num_sites, num_sites))
+    for i in range(num_sites):
+        for j in range(num_sites):
+            if i != j:
+                distance_matrix[i, j] = geodesic(coords[i], coords[j]).km
+    return distance_matrix
+
+def create_batches(coords, min_distance_km):
+    """Create batches based on the minimum distance constraint."""
+    distance_matrix = calculate_distance_matrix(coords)
+    remaining_sites = list(range(len(coords)))
+    batches = []
+    
+    while remaining_sites:
+        batch = [remaining_sites.pop(0)]  # Start with the first site
+        while True:
+            # Find the nearest site to the last added site in the batch that is at least min_distance_km away
+            last_site = batch[-1]
+            valid_sites = [site for site in remaining_sites if distance_matrix[last_site, site] >= min_distance_km]
+            
+            if not valid_sites:
+                break
+            
+            distances_to_last_site = {site: distance_matrix[last_site, site] for site in valid_sites}
+            next_site = min(distances_to_last_site, key=distances_to_last_site.get)
+            batch.append(next_site)
+            remaining_sites.remove(next_site)
+        
+        batches.append(batch)
+    
+    return batches
+
+# Create batches
 min_distance_km = 10
+batches = create_batches(coords, min_distance_km)
 
-# Convert coordinates to distances (in kilometers)
-def calculate_distance(coord1, coord2):
-    return geodesic(coord1, coord2).km
-
-# Use fclusterdata from scipy to cluster based on minimum distance
-clusters = fclusterdata(sites, t=min_distance_km, criterion='distance', metric=lambda u, v: geodesic(u, v).km)
-
-# Group sites by clusters
-from collections import defaultdict
-batches = defaultdict(list)
-for site, cluster_id in zip(sites, clusters):
-    batches[cluster_id].append(site)
-
-# Output the batches
-for cluster_id, batch in batches.items():
-    print(f"Batch {cluster_id}:")
+# Add cluster labels to DataFrame
+df['cluster'] = -1
+for cluster_id, batch in enumerate(batches):
     for site in batch:
-        print(f" - {site}")
+        df.at[site, 'cluster'] = cluster_id
+
+# Display the results
+for cluster_id, batch in df.groupby('cluster'):
+    print(f"Batch {cluster_id}:")
+    print(batch[['longitude', 'latitude']])
+    print()
